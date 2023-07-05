0f6ce56c1e800ff4b518b12ad664fb0bd162a5ac
gaetanochiriaco <g.chiriaco@hotmail.it>
2023-07-05 11:47:48 +0200

    added gradcam

--- food_recognition/cam/__init__.py
+++ food_recognition/cam/__init__.py
@@ -1,3 +1,4 @@
 from food_recognition.cam.get_cam import get_output_and_cam
 from food_recognition.cam.eigen_cam import EigenCAM
+from food_recognition.cam.grad_cam import GradCAM
 from food_recognition.cam.utils import *
